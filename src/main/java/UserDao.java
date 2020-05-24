ead6feb2cd9d4997da4320552959125e7da0ae5f
Akshit Gupta <akg009@ucsd.edu>
2020-05-23 22:00:40 -0700

    Suppressed redundant warnings

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -1,4 +1,5 @@
 public class UserDao {
+    @SuppressWarnings({"unused", "RedundantThrows"})
     public User getUserOrNull(String email, String password) throws DatabaseDownException {
         throw new RuntimeException("Not implemented");
     }
